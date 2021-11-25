9af718e27b519863c87403b8eae926e4f46fe9f9
Thiago Silva <thiago.silva@imd.ufrn.br>
2021-11-24 23:55:04 -0300

    🗃️ Perform database related changes
    - Exports 'User' module
    - Create 'users' database
    - Add a user into 'users' database
    - Sequelize configuration: models
    - Database configuration: postgres

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
 const routes = require('express').Router()
 
+const { User } = require('./app/models')
+
+User.create({
+  name: 'Thiago',
+  email: 'thiago@email.com',
+  password_hash: '139846317248',
+})
+
 module.exports = routes
